875c59765425429226eb7742d5c964efe98c9ea3
zengxiaosen <919451814@qq.com>
2018-06-09 02:32:18 -0400

    debug

--- apps/fwd/src/main/java/org/onosproject/fwd/ReactiveForwarding.java
+++ apps/fwd/src/main/java/org/onosproject/fwd/ReactiveForwarding.java
@@ -1531,7 +1531,7 @@ public class ReactiveForwarding {
 
                     //log.info("resultScore: " + resultScore);
                     //there are some problem
-                    double resultScore = feature_ChokePointRestBandWidth * 5 + feature_pathMeanRestBw * 2 + feature_preAddFlowToThisPath_AllStandardDeviation * 3;
+                    double resultScore = feature_ChokePointRestBandWidth * 5 + feature_pathMeanRestBw * 4 + feature_preAddFlowToThisPath_AllStandardDeviation * 1;
                     //double resultScore = (ChokePointRestBandWidth*0.4 + pathMeanRestBw*0.2 + 2)*10/(0.4*preAddFlowToThisPath_AllStandardDeviation + 1);
                     //log.info("resultScore: "+ resultScore);
 
