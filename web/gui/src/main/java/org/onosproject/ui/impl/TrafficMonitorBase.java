9cfd011c32c185b67c9ceb01563c01a5c5d49fd2
zengxiaosen <919451814@qq.com>
2018-06-23 04:23:30 -0400

    debug

--- web/gui/src/main/java/org/onosproject/ui/impl/TrafficMonitorBase.java
+++ web/gui/src/main/java/org/onosproject/ui/impl/TrafficMonitorBase.java
@@ -1096,7 +1096,7 @@ public abstract class TrafficMonitorBase extends AbstractTopoMonitor {
 
             //log.info("resultScore: " + resultScore);
             //there are some problem
-            double resultScore = feature_ChokePointRestBandWidth * 4 + feature_pathMeanRestBw * 4 + feature_preAddFlowToThisPath_AllStandardDeviation * 2;
+            double resultScore = feature_ChokePointRestBandWidth * 4 + feature_pathMeanRestBw * 5 + feature_preAddFlowToThisPath_AllStandardDeviation * 1;
 
             //double resultScore = (ChokePointRestBandWidth*0.4 + pathMeanRestBw*0.2 + 2)*10/(0.4*preAddFlowToThisPath_AllStandardDeviation + 1);
             //log.info("resultScore: "+ resultScore);
