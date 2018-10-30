a6892be7ebce125c37e8ce1f32ab488df6939323
zengxiaosen <919451814@qq.com>
2018-10-30 09:14:24 -0400

    fix bug

--- web/gui/src/main/java/org/onosproject/ui/impl/TrafficMonitorBase.java
+++ web/gui/src/main/java/org/onosproject/ui/impl/TrafficMonitorBase.java
@@ -1200,7 +1200,7 @@ public abstract class TrafficMonitorBase extends AbstractTopoMonitor {
             double fPathMeanRestBw = (double)(Math.log((double)pathMeanRestBw + 1));
             //double fAllRestBwSdAfterPreAdd = 1.0/(double)(Math.log((double)AllRestBWSdAfterPreAdd + 1) + 0.1);
             double fAllRestBwSdAfterPreAdd = (double)(Math.log((double)AllRestBWSdAfterPreAdd + 1));
-            double resultScore = fChokeLinkRestBw * 5 + fPathMeanRestBw * 3 - fAllRestBwSdAfterPreAdd * 2;
+            double resultScore = fChokeLinkRestBw * 5 + fPathMeanRestBw * 4 - fAllRestBwSdAfterPreAdd * 1;
             //log
             log.info("ChokePointRestBandWidth: " + ChokePointRestBandWidth);
             log.info("pathMeanRestBw: " + pathMeanRestBw);
