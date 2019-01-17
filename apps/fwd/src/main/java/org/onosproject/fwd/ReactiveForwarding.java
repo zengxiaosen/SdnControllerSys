777c4d6f1354a81a8902e53c94ecd0541a2714c4
zengxiaosen <919451814@qq.com>
2019-01-17 16:29:06 +0800

    fix bug

--- apps/fwd/src/main/java/org/onosproject/fwd/ReactiveForwarding.java
+++ apps/fwd/src/main/java/org/onosproject/fwd/ReactiveForwarding.java
@@ -1139,7 +1139,7 @@ public class ReactiveForwarding {
                 double fChokeLinkRestBw = (double)(Math.log((double)ChokePointRestBandWidth + 1));
                 double fPathMeanRestBw = (double)(Math.log((double)pathMeanRestBw + 1));
                 double fAllRestBwSdAfterPreAdd = (double)(Math.log((double)AllRestBWSdAfterPreAdd + 1) );
-                double resultScore = fChokeLinkRestBw * 5 + fPathMeanRestBw * 3 - fAllRestBwSdAfterPreAdd * 2;
+                double resultScore = fChokeLinkRestBw * 5 + fPathMeanRestBw * 2 - fAllRestBwSdAfterPreAdd * 3;
 
                 Path prePickPath = checkLeadBackSrc(path, srcPort);
                 //if lead back to the src port
