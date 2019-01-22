e0d50eb70b7d0e233f8ea749bc4c0d4857a045a8
zengxiaosen <919451814@qq.com>
2019-01-22 11:57:26 +0800

    fix bug

--- apps/fwd/src/main/java/org/onosproject/fwd/ReactiveForwarding.java
+++ apps/fwd/src/main/java/org/onosproject/fwd/ReactiveForwarding.java
@@ -650,7 +650,7 @@ public class ReactiveForwarding {
 
             Set<Path> PathsChoise = Sets.newHashSet();
             //TODO to a service ...
-            int choise = 0;
+            int choise = 2;
             if(choise == 0){
                 Set<Path> PathsFSEM = PathsDecisionFESM(paths, pkt.receivedFrom().port());
                 PathsChoise = PathsFSEM;
